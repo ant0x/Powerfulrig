0e338d448dd15ffc9bd39d521da468d60339d02f
ant0x <43486318+ant0x@users.noreply.github.com>
2019-07-25 18:08:08 +0200

    ciccio la vai co sto commit

---
+++ src/Model/servaxxsa.java
@@ -0,0 +1,51 @@
+package Model;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servaxxsa
+ */
+@WebServlet("/servaxxsa")
+public class servaxxsa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     * adds+dsa
+     * asd
+     * dsa
+     * dsa
+     * sad
+     * ads
+     * ads
+     * asd
+     * dsa
+     * d
+     * sa
+     */
+    public servaxxsa() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
